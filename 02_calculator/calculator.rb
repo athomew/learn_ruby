7e61150397ec03cf13ea33625d9475aabb3880d0
Aline Thome <alinethomew@gmail.com>
2017-07-03 18:09:26 -0500

    Do the required exercises in 02.

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,17 @@
 #write your code here
+
+def add n1, n2
+  n1 + n2
+end
+
+def subtract n1, n2
+  n1 - n2
+end
+
+def sum array
+  sum = 0
+  array.length.times do |i|
+    sum = sum + array[i]
+  end
+  sum
+end 

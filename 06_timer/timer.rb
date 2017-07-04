17344c411805d4dfb67b002ba713747ed36d12c6
Aline Thome <alinethomew@gmail.com>
2017-07-03 20:03:25 -0500

    Finish 06.

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,37 @@
 class Timer
   #write your code here
+  attr_accessor :seconds
+  attr_accessor :minutes
+  attr_accessor :hours
+
+  def initialize
+    @seconds = 0
+    @minutes = 0
+    @hours = 0
+  end
+
+  def time_string
+    convert
+    stringify(@hours) + ":" + stringify(@minutes) + ":" + stringify(@seconds)
+  end
+
+  def convert
+    if seconds > 59
+      @minutes = @seconds / 60
+      @seconds = @seconds - (@minutes*60)
+    end
+    if minutes > 59
+      @hours = @minutes / 60
+      @minutes = @minutes - (@hours * 60)
+    end
+  end
+
+  def stringify time
+    if time < 10
+      "0" + time.to_s
+    else
+      time.to_s
+    end
+  end
+
 end

d1effd841bc77bd2351b3bd488d58a76444074c9
Aline Thome <alinethomew@gmail.com>
2017-07-03 19:35:31 -0500

    Finish 05.

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,21 @@
 class Book
-# write your code here
+
+  attr_reader :title
+
+  def name=(str)
+    @name = titleize(str)
+  end
+
+  def titleize phrase
+    # Still missing a bunch of words in the 'little words' list.
+    little_words = ["and", "the", "a", "an", "for", "but", "or", "over", "in", "out", "for", "from", "under"]
+    words = phrase.split
+    words.length.times do |i|
+      unless ((little_words.include? words[i]) and (i > 0))
+        words[i] = words[i].capitalize
+      end
+    end
+    words.join(" ")
+  end
+
 end
